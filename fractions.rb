7d0e360d6325b2c8f98dbb008cb431bd83797ea4
gl46372 <gl46372@students.westport,k12,ct.us>
2014-11-06 12:36:51 -0500

    Initial Commit

---
+++ fractions.rb
@@ -0,0 +1,45 @@
+def simplify(n3, d3)
+  gcd = n3.gcd(d3)
+  n3new = n3 / gcd
+  d3new = d3 / gcd
+  nd_array = [n3new, d3new]
+end
+
+def multiply(n1, d1, n2, d2)
+  n3 = n1 * n2
+  d3 = d1 * d2
+  simplify(n3, d3)
+end
+
+def divide(n1, d1, n2, d2)
+  n3 = n1 * d2
+  d3 = d1 * n2
+  simplify(n3, d3)
+end
+
+def add(n1, d1, n2, d2)
+  n1new = n1 * d2
+  n2new = n2 * d1
+  dnew = d2 * d1
+  n3 = n1new + n2new
+  d3 = dnew
+  simplify(n3, d3)
+end
+
+def subtract(n1, d1, n2, d2)
+  n1new = n1 * d2
+  n2new = n2 * d1
+  dnew = d2 * d1
+  n3 = n1new - n2new
+  d3 = dnew
+  simplify(n3, d3)
+end
+
+def output_fraction(nd_array)
+  n, d = nd_array
+  puts n.to_s + '/' + d.to_s
+end
+
+if __FILE__ == $0
+output_fraction(multiply(48, 2, 3, 6))
+end
